cc9e0ac65a527015deb55fe3de3a8f2c44c888dc
Daniel Montesinos <daniel.montesinos@e2ycommerce.com>
2021-03-16 14:52:35 +0100

    Finish unit tests. Start first lesson of functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,68 +7,70 @@ const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
 
 suite("Functional Tests", function () {
-  suite("Integration tests with chai-http", function () {
-    // #1
-    test("Test GET /hello with no name", function (done) {
-      chai
-        .request(server)
-        .get("/hello")
-        .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, "hello Guest");
-          done();
+    suite("Integration tests with chai-http", function () {
+        // #1
+        test("Test GET /hello with no name", function (done) {
+            chai
+                .request(server)
+                .get("/hello")
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.text, "hello Guest");
+                    done();
+                });
+        });
+        // #2
+        test("Test GET /hello with your name", function (done) {
+            chai
+                .request(server)
+                .get("/hello?name=xy_z")
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.text, "hello xy_z");
+                    done();
+                });
+        });
+        // #3
+        test('send {surname: "Colombo"}', function (done) {
+            chai
+                .request(server)
+                .put("/travellers")
+                .send({ surname: "Colombo" })
+                .end(function (err, res) {
+                    console.log(JSON.stringify(res.body));
+                    assert.equal(res.status, 200);
+                    assert.equal(res.type, 'application/json');
+                    assert.deepPropertyVal(res.body, surname, 'Colombo');
+                    done();
+                });
+        });
+        // #4
+        test('send {surname: "da Verrazzano"}', function (done) {
+            assert.fail();
+
+            done();
         });
     });
-    // #2
-    test("Test GET /hello with your name", function (done) {
-      chai
-        .request(server)
-        .get("/hello?name=xy_z")
-        .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, "hello xy_z");
-          done();
-        });
-    });
-    // #3
-    test('send {surname: "Colombo"}', function (done) {
-      chai
-        .request(server)
-        .put("/travellers")
-
-        .end(function (err, res) {
-          assert.fail();
-
-          done();
-        });
-    });
-    // #4
-    test('send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
-
-      done();
-    });
-  });
 });
 
 const Browser = require("zombie");
 
 suite("Functional Tests with Zombie.js", function () {
 
-  suite('"Famous Italian Explorers" form', function () {
-    // #5
-    test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
-      browser.fill("surname", "Colombo").pressButton("submit", function () {
-        assert.fail();
+    suite('"Famous Italian Explorers" form', function () {
+        // #5
+        test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
+            browser.fill("surname", "Colombo").pressButton("submit", function () {
+                assert.fail();
 
-        done();
-      });
-    });
-    // #6
-    test('submit "surname" : "Vespucci" - write your e2e test...', function (done) {
-      assert.fail();
+                done();
+            });
+        });
+        // #6
+        test('submit "surname" : "Vespucci" - write your e2e test...', function (done) {
+            assert.fail();
 
-      done();
+            done();
+        });
     });
-  });
 });
