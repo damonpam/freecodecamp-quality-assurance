b72bed690838b8e17399d69a996441d9f01beb05
Daniel Montesinos <daniel.montesinos@e2ycommerce.com>
2021-03-17 13:01:19 +0100

    finish functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -40,45 +40,57 @@ suite("Functional Tests", function () {
                     console.log(JSON.stringify(res.body));
                     assert.equal(res.status, 200);
                     assert.equal(res.type, 'application/json');
-                    assert.deepPropertyVal(res.body, surname, 'Colombo');
+                    assert.equal(res.body.name, "Cristoforo");
+                    assert.equal(res.body.surname, "Colombo");
                     done();
                 });
         });
         // #4
-    test('send {surname: "da Verrazzano"}', function (done) {
-      chai
-        .request(server)
-        .put("/travellers")
-        .send({"surname": "da Verrazzano"})
-        .end(function (err, res) {
-          assert.strictEqual(res.status, 200);
-          assert.strictEqual(res.type, "application/json");
-          assert.strictEqual(res.body.name, "Giovanni");
-          assert.strictEqual(res.body.surname, "da Verrazzano");      
-          done();
+        test('send {surname: "da Verrazzano"}', function (done) {
+            chai
+                .request(server)
+                .put("/travellers")
+                .send({ "surname": "da Verrazzano" })
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.type, "application/json");
+                    assert.equal(res.body.name, "Giovanni");
+                    assert.equal(res.body.surname, "da Verrazzano");
+                    done();
+                });
         });
-    });
     });
 });
 
 const Browser = require("zombie");
+Browser.site = 'https://boilerplate-mochachai-3.damonpam.repl.co';
 
 suite("Functional Tests with Zombie.js", function () {
+    const browser = new Browser();
+    suiteSetup(function (done) {
+        return browser.visit('/', done);
+    });
 
     suite('"Famous Italian Explorers" form', function () {
         // #5
         test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
             browser.fill("surname", "Colombo").pressButton("submit", function () {
-                assert.fail();
-
+                browser.assert.success();
+                browser.assert.text('span#name', 'Cristoforo');
+                browser.assert.text('span#surname', 'Colombo');
+                browser.assert.element('span#dates', 1);
                 done();
             });
         });
         // #6
         test('submit "surname" : "Vespucci" - write your e2e test...', function (done) {
-            assert.fail();
-
-            done();
+            browser.fill("surname", "Vespucci").pressButton("submit", function () {
+                browser.assert.success();
+                browser.assert.text('span#name', 'Amerigo');
+                browser.assert.text('span#surname', 'Vespucci');
+                browser.assert.element('span#dates', 1);
+                done();
+            });
         });
     });
 });
