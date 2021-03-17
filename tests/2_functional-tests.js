db6bb379fefb8dabe29751c3c9990f55620d6105
Daniel Montesinos <daniel.montesinos@e2ycommerce.com>
2021-03-17 13:05:02 +0100

    fix app url

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -63,7 +63,7 @@ suite("Functional Tests", function () {
 });
 
 const Browser = require("zombie");
-Browser.site = 'https://boilerplate-mochachai-3.damonpam.repl.co';
+Browser.site = 'https://freecodecamp-quality-assurance.damonpam.repl.co';
 
 suite("Functional Tests with Zombie.js", function () {
     const browser = new Browser();
